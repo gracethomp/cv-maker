b2916340ec4760b8af68cb01c6fbd26fc96eadc1
gracethomp <gracetomlinson26@gmail.com>
2024-07-26 19:29:23 +0300

    setup "now" font family for pdf

--- src/ui/document/MyDocument.tsx
+++ src/ui/document/MyDocument.tsx
@@ -5,9 +5,12 @@ import {
   Image,
   View,
   StyleSheet,
+  Text,
 } from "@react-pdf/renderer";
 import { FC } from "react";
 import photo from "../../image/photo.png";
+import { useAppSelector } from "../../hooks/redux";
+import "../../utils/fontSetup";
 
 const styles = StyleSheet.create({
   head: {
@@ -26,21 +29,40 @@ const styles = StyleSheet.create({
   photo: {
     objectFit: "cover",
     objectPosition: "top",
-    borderRadius: '100%',
+    borderRadius: "100%",
+  },
+  mainInfo: {
+    display: "flex",
+    flexDirection: "row",
+    gap: 50,
+    fontFamily: 'Now',
+  },
+  rightPart: {
+    flex: 1,
+  },
+  name: {
+    fontSize: 35,
+    fontWeight: 'bold',
+    color: '#E23459'
   },
 });
 
 const MyDocument: FC = () => {
+  const firstName = useAppSelector((state) => state.dataReducer.firstName);
   return (
     <PDFViewer className="h-full flex-1">
       <Document>
         <Page size="A4">
           <View style={styles.head} />
           <View style={styles.section}>
-            <View style={styles.photoSection}>
-              <Image src={photo} style={styles.photo}></Image>
+            <View style={styles.mainInfo}>
+              <View style={styles.photoSection}>
+                <Image src={photo} style={styles.photo}></Image>
+              </View>
+              <View style={styles.rightPart}>
+                <Text style={styles.name}>{firstName.toUpperCase()}</Text>
+              </View>
             </View>
-
           </View>
         </Page>
       </Document>
--- src/ui/form/Form.tsx
+++ src/ui/form/Form.tsx
@@ -1,11 +1,25 @@
 import { TextField } from "@mui/material";
 import { FC } from "react";
+import { useAppDispatch, useAppSelector } from "../../hooks/redux";
+import { dataSliceActions } from "../../store/slices/dataSlice";
 
 const Form: FC = () => {
+  const dispatch = useAppDispatch();
+  const firstName = useAppSelector((state) => state.dataReducer.firstName);
+
   return (
     <div className="m-10 flex-1">
       <div>
-        <TextField id="outlined-basic" label="First Name" variant="outlined" className='w-full'/>
+        <TextField
+          id="first-name"
+          label="First Name"
+          variant="outlined"
+          className="w-full"
+          value={firstName}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            dispatch(dataSliceActions.setFirstName(e.target.value))
+          }
+        />
       </div>
     </div>
   );
